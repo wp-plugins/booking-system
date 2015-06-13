decbf97e77a1288d977b3a64b9b79fa558963efe
Otto42 <Otto42@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-13 15:46:37 +0000

    Split opening comment of main plugin file.
    git-svn-id: https://plugins.svn.wordpress.org/booking-system/trunk@1180257 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- dopbs.php
+++ dopbs.php
@@ -6,7 +6,9 @@ Plugin URI: https://wordpress.org/plugins/booking-system/
 Description: Transform your WordPress website into a booking/reservation system. If you like this plugin, feel free to rate it five stars at <a href="https://wordpress.org/support/view/plugin-reviews/booking-system" target="_blank">Wordpress</a> in Reviews section. If you encounter any problems please do not give a low rating but <a href="https://wordpress.org/support/plugin/booking-system" target="_blank">visit</a> our <a href="https://wordpress.org/support/plugin/booking-system" target="_blank">Support</a> first so we can help you.
 Author: Dot on Paper
 Author URI: http://www.dotonpaper.net
+*/
 
+/*
 Change log:
 
         2.0 (2015-03-28)
